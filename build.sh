b889a5581a5a5a75c70932eac81862b5df653a3b
Nuno Levezinho <nuno.levezinho@live.com.pt>
2021-03-26 03:09:13 +0000

    Added unix build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NPROC=$(nproc)
+
+mkdir build
+
+cd build || exit
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOATPP_BUILD_TESTS=OFF -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF --target HttpServer ..
+
+cmake --build . --target HttpServer -- -j $NPROC
