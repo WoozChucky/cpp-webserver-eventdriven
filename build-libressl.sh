fc420c0520550f3235c3aec75e6c20edc837650a
Nuno Levezinho <nuno.levezinho@live.com.pt>
2021-03-26 03:20:24 +0000

    added build libressl script

---
+++ build-libressl.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+NPROC=$(nproc)
+
+cd LibreSSL || exit
+
+sudo sh autogen.sh
+
+sudo ./configure
+
+mkdir build
+
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF ..
+
+make -j $NPROC
+
+sudo make install -j $NPROC
+
+cd ..
