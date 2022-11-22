ff59fe78875fa652f7caff4eeec2ce505d992829
Nuno Levezinho <nuno.levezinho@live.com.pt>
2022-11-22 01:59:03 +0000

    Fixed CMake --targe usage in CMake 3.20.x >

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ mkdir build
 
 cd build || exit
 
-cmake -DCMAKE_BUILD_TYPE=Release -DOATPP_BUILD_TESTS=OFF -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF --target HttpServer ..
+cmake -DCMAKE_BUILD_TYPE=Release -DOATPP_BUILD_TESTS=OFF -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF  ..
 
 cmake --build . --target HttpServer -- -j $NPROC
